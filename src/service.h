52e54593981adbf95f4000e3cc24675cca74d016
stephenv <stephenv@localhost>
2013-02-21 21:46:55 +0000

    Add note about disabled compilation warnings to src/service.h, set status to start pending during service start, not processing the nodeArgs options parameter to the add() function, example program was not passing programArgs correctly

--- src/service.h
+++ src/service.h
@@ -1,6 +1,21 @@
 #ifndef SERVICE_H
 #define SERVICE_H
 
+/**
+ ** The following warnings are displayed during compilation on win32 platforms
+ ** using node-gyp:
+ **
+ **  - C++ exception handler used, but unwind semantics are not enabled.
+ **  - no definition for inline function 'v8::Persistent<T> \
+ **       v8::Persistent<T>::New(v8::Handle<T>)'
+ **
+ ** There don't seem to be any issues which would suggest these are real
+ ** problems, so we've disabled them for now.
+ **/
+#ifdef _WIN32
+#pragma warning(disable:4506;disable:4530)
+#endif
+
 #include <node.h>
 
 using namespace v8;
